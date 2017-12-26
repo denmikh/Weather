434f95e1790c9e8b5259033ffde82cda49320b2b
denmikh <denmih61@gmail.com>
2017-12-26 17:39:45 +0300

    new file(css, js), map none, new name function,

--- index.php
+++ index.php
@@ -16,61 +16,12 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 50%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      .controls {
-        margin-top: 10px;
-        border: 1px solid transparent;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        height: 32px;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-      }
+    <script src="js/common.js"></script>
+    <script src="js/weather.js"></script>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
 
-      #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 300px;
-      }
-
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
-
-      .pac-container {
-        font-family: Roboto;
-      }
-
-      #type-selector {
-        color: #fff;
-        background-color: #4d90fe;
-        padding: 5px 11px 0px 11px;
-      }
-
-      #type-selector label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-      }
-    </style>
   </head>
+
   <body>
     <input id="pac-input" class="controls" type="text" placeholder="Enter a location">
     <div id="type-selector" class="controls">
@@ -91,147 +42,10 @@
     <div id="map"></div>
 
     <script>
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 47.23571369999999, lng: 39.701505},
-          zoom: 13
-        });
-        var input = /** @type {!HTMLInputElement} */(
-            document.getElementById('pac-input', 'butt'));
-
-        var types = document.getElementById('type-selector');
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map);
-
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-          map: map,
-          anchorPoint: new google.maps.Point(0, -29)
-        });
-
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          
-          getweather(place.geometry.location.lat(), place.geometry.location.lng());
-
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setIcon(/** @type {google.maps.Icon} */({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-          }));
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
-
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-          infowindow.open(map, marker);
-        });
-
-        // Sets a listener on a radio button to change the filter type on Places
-        // Autocomplete.
-        function setupClickListener(id, types) {
-          var radioButton = document.getElementById(id);
-          radioButton.addEventListener('click', function() {
-            autocomplete.setTypes(types);
-          });
-        }
-
-        setupClickListener('changetype-all', []);
-        setupClickListener('changetype-address', ['address']);
-        setupClickListener('changetype-establishment', ['establishment']);
-        setupClickListener('changetype-geocode', ['geocode']);
-      }
-
-      function getweather(lat, lng){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lng+'&APPID=1a48d78a9c81844bb059e91f7ecd119f', true);
-        xhr.send();
-        // console.log(xhr)
-        xhr.onreadystatechange = function() {
-          if (this.readyState != 4) return;
-            var data = eval("("+xhr.responseText+")");
-            // console.log(data);
-            data.list.forEach(function(item) {
-              addItem(item);
-                });
-              };
-              document.getElementById('asd').innerHTML = '';
-              // $("#asd").empty();
-            
-            };
-    
-
-      // function getweather(lat, lng){
-      //     $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lng+"&APPID=1a48d78a9c81844bb059e91f7ecd119f", function (data) {
-      //       console.log(data)
-      //       data.list.forEach(function(item) {
-      //         addItem(item);
-      //       });
-      //     });
-      //     $("#asd").empty();
-      //   };
-
-        
-        function addtweatherString(weatherItems){
-          var str = "" ;
-          weatherItems.forEach(function(items) {
-            // if(str != ""){
-            //   str = str + ",";
-            // }
-            str = str + items.description;
-            //  console.log(items);
-          });
-          return str;
-        };
-
-       function addItem(item){
-          // console.log(item);
-          var date = item.dt_txt;
-          var temp = Math.round(item.main.temp - 273);
-          var weather = addtweatherString(item.weather);
-          var dat = "<tr><td>"+date+"</td><td>"+temp+"</td><td>"+weather+"</td><tr>";
-          document.getElementById('asd').innerHTML = document.getElementById('asd').innerHTML + dat;
-          
-          // $("#asd").append(dat);
-       };
-
+      initMap();
     </script>
-<div id="tag"></div>
 
-<div class="table-responsive col-md-6">
+<div id="asd" class="table-responsive col-md-6">
 	<table class="table table-hover table-bordered table-condensed ">
 		<thead>
 			<tr class="active">
@@ -240,8 +54,7 @@
 				<th>Weather condition</th>
 			</tr>
 		</thead>
-		<tbody id="asd">
-
+		<tbody id="table">
 		</tbody>
 	</table>
 </div>
